3d172779679ce13a8dbeeca70abb4be070fb164a
Mia Delpriora <82186285+mdelpriora@users.noreply.github.com>
2025-02-20 10:44:15 -0500

    Add files via upload

---
+++ AboutMyself.java
@@ -0,0 +1,17 @@
+
+public class AboutMyself 
+{
+
+	public static void main(String[] args) 
+	{
+		// Part 1 of question 2
+		System.out.println("My name is Mia Delpriora");
+		System.out.println("I was born in China");
+		System.out.println("I like drawing , painting and listening to music");
+		
+		// Part 2 of question 2
+		System.out.println("My name is Mia Delpriora\nI was born in China\nI like drawing , painting and listening to music");
+		
+	}
+
+}
